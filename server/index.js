0d9a2d4fca13d27e272dbb3268995ac4eaed090c
Fiodor Gorobet <iamfiogo@gmail.com>
2023-01-02 14:00:25 +0100

    Initial commit. bootstraping server

---
+++ server/index.js
@@ -0,0 +1,29 @@
+import express from 'express'
+import mongoose from 'mongoose'
+import cors from 'cors'
+import dotenv from 'dotenv'
+import helmet from 'helmet'
+import morgan from 'morgan'
+
+import clientRoutes from './routes/client'
+import generalRoutes from './routes/general'
+import managementRoutes from './routes/management'
+import salesRoutes from './routes/sales'
+
+
+// CONFIGS
+dotenv.config()
+
+const app = express()
+app.use(express.json())
+app.use(helmet())
+app.use(helmet.crossOriginResourcePolicy({ policy: 'cross-origin' }))
+app.use(morgan('common'))
+app.use(express.urlencoded({ extended: false }))
+app.use(cors())
+
+// ROUTES
+app.use('/client', clientRoutes)
+app.use('/general', generalRoutes)
+app.use('/management', managementRoutes)
+app.use('/sales', salesRoutes)
